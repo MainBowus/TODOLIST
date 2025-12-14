2dbafa8919624952b980bee8dcf509d1a5a7bc13
MainBowUs <marklnwzaza40@gmail.com>
2025-12-14 12:02:27 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express  from "express";
 import dotenv from "dotenv";
+import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
+
 
 dotenv.config();
 
@@ -8,9 +10,12 @@ const app = express();
 const PORT = 3000;
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+//app.get("/", (req, res) => {
+  //res.send("Server is running");
+//});
+
+app.use("/api/todos", todoRoutes);
+
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on http://localhost:${PORT}`);
